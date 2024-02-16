4ba48960809f4639518ddc24202cd620e2ae3e44
HC <51252433+bchou9@users.noreply.github.com>
2024-02-16 00:32:49 -0800

    fixed code to create code coverage data

--- getCodeCovReport.py
+++ getCodeCovReport.py
@@ -3,60 +3,61 @@ import json
 import requests
 import urllib
 import urllib.request, json
+import pandas as pd
 
-CODECOV_ENDPOINT = "https://codecov.io/api/v2/{}/{}"
-TOKEN_NAME = "7848dd6f-5308-43f6-a02f-e10e31118854"
-CODECOV_HEADERS = {
-    'Authorization': 'bearer {}'.format(TOKEN_NAME)
-}
-
-def _display_coverage(endpoint):
+def _display_codecov(platform, username, repo_name):
+    CODECOV_ENDPOINT = "https://codecov.io/api/v2/{}/{}"
+    TOKEN_NAME = "7848dd6f-5308-43f6-a02f-e10e31118854"
+    CODECOV_HEADERS = {
+        'Authorization': 'bearer {}'.format(TOKEN_NAME)
+    }
+    endpoint = CODECOV_ENDPOINT.format(platform, username)
+    endpoint = endpoint + "/repos/" + repo_name + "/commits"
     response = requests.get(
         endpoint,
         headers=CODECOV_HEADERS,
     )
     content = json.loads(response.content)
-    commit = None
-    if content['count'] == 0:
-        return
-
-    for commit in content['results']:
-        totals = commit['totals']
-        if totals is None:
-            continue
-
-        coverage = totals.get('coverage', None)
-        if coverage is not None:
-            print('{}%'.format(coverage))
+    if response.status_code == 200:
+        commit = None
+        if content['count'] == 0:
             return
+
+        for commit in content['results']:
+            totals = commit['totals']
+            if totals is None:
+                continue
+
+            coverage = totals.get('coverage', None)
+            if coverage is not None:
+                print('{}%'.format(coverage))
+                return
 
     print('No coverage found on any commit')
 
-def _display_coverall(endpoint):
-    response = requests.get(
-        endpoint
-    )
-    print(response)
-
-def display_coverage_dashboard(platform, username, repo_name):
-    global CODECOV_ENDPOINT
-    endpoint = CODECOV_ENDPOINT.format(platform, username)
-    endpoint = endpoint + "/repos/" + repo_name + "/commits"
-    _display_coverage(endpoint)
-
+def _display_coverall(platform, username, repo_name):
     coverall_endpoint = "https://coveralls.io/github/{}/{}.json"
-
     coverall_endpoint = coverall_endpoint.format(username, repo_name)
     try:
         with urllib.request.urlopen(coverall_endpoint) as url:
             data = json.load(url)
-            print(data['covered_percent'])
+            if data is not None:
+                print(data['covered_percent'])
     except urllib.error.HTTPError as e:
         print("repo does not use coverall")
+
+def display_coverage_dashboard(platform, username, repo_name):
+    _display_codecov(platform, username, repo_name)
+    _display_coverall(platform, username, repo_name)
 
 
 if __name__=="__main__":
     platform = 'github'
-    username = 'codecov'
-    repo_name = 'codecov-ats'
-    display_coverage_dashboard(platform, username, repo_name)
+    # read csv
+    data = pd.read_csv('github-ranking-2024-02-15.csv')
+    data_dict = data.to_dict(orient='records')
+    data_dict = [item for item in data_dict if item['language'] == 'JavaScript' or item['language'] == 'TypeScript']
+    for item in data_dict:
+        username = item['username']
+        repo_name = item['repo_name']
+        display_coverage_dashboard(platform, username, repo_name)
