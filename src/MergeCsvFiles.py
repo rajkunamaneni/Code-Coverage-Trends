0c84e2cbe22d6255e3a971ff4ff92ab1c8c9cb64
HC <51252433+bchou9@users.noreply.github.com>
2024-03-12 23:58:27 -0700

    Create function for get user and repo name

--- src/MergeCsvFiles.py
+++ src/MergeCsvFiles.py
@@ -1,14 +1,6 @@
-import json
-import logging
-import requests
-import DateUtil
-import time
 import pandas as pd
 import csv
 import os
-import GlobalVarSetting as globalvar
-import itertools
-import GetContributorData
 import glob
 
 def strip_timestamp(pop_csv_dict_of_df):
@@ -22,27 +14,30 @@ def split_on_occurence_char(pr_csv_filename_no_suffix, delimiter, position):
     str_groups = pr_csv_filename_no_suffix.split(delimiter)
     return '_'.join(str_groups[:position]), '_'.join(str_groups[position:])
 
+def get_username_reponame_from_filename(pr_csv_file, pr_file_prefix):
+    pr_csv_df = pd.read_csv(pr_csv_file)
+    pr_csv_filename = os.path.basename(pr_csv_file)
+    pr_csv_filename_no_prefix = pr_csv_filename.removeprefix(pr_file_prefix)
+    pr_csv_filename_no_suffix = pr_csv_filename_no_prefix.removesuffix('.csv')
+
+    underscore_count = pr_csv_filename_no_suffix.count('_')
+    [username, repository] = [None, None]
+
+    if underscore_count == 1:
+        [username, repository] = pr_csv_filename_no_suffix.split('_')
+    elif underscore_count > 1:
+        [username, repository] = split_on_occurence_char(pr_csv_filename_no_suffix, '_', 1)
+        print(f'repo used bad naming practices: {username}/{repository}')
+    else:
+        raise ValueError
+    return pr_csv_df, username, repository
+
 def combine_pr_with_pop_report(pr_history_csv_file_list, pr_file_prefix, pop_csv_dict_of_df):
     for pr_csv_file in pr_history_csv_file_list:
         try:
             if pr_file_prefix in pr_csv_file:
-                pr_csv_df = pd.read_csv(pr_csv_file)
-                pr_csv_filename = os.path.basename(pr_csv_file)
-                pr_csv_filename_no_prefix = pr_csv_filename.removeprefix('prs_per_day_')
-                pr_csv_filename_no_suffix = pr_csv_filename_no_prefix.removesuffix('.csv')
+                pr_csv_df, username, repository = get_username_reponame_from_filename(pr_csv_file, pr_file_prefix)
 
-                underscore_count = pr_csv_filename_no_suffix.count('_')
-                [username, repository] = [None, None]
-
-                if underscore_count == 1:
-                    [username, repository] = pr_csv_filename_no_suffix.split('_')
-                elif underscore_count > 1:
-                    [username, repository] = split_on_occurence_char(pr_csv_filename_no_suffix, '_', 1)
-                    print(f'repo used bad naming practices: {username}/{repository}')
-                else:
-                    raise ValueError
-
-                pr_count_aligned_date_list = []
                 for idx, pop_date_commit_dict_inst in enumerate(pop_csv_dict_of_df):
                     if (pop_date_commit_dict_inst['Username'] == username) \
                             and (pop_date_commit_dict_inst['Repository'] == repository):
@@ -55,7 +50,6 @@ def combine_pr_with_pop_report(pr_history_csv_file_list, pr_file_prefix, pop_csv
         except ValueError:
             print(f'repo used invalid naming practices, skipping: {pr_csv_file}')
             pass
-
 
 def write_to_csv(pop_report_csv_file, pop_csv_dict_of_df, pop_report_csv_path, new_prefix):
     original_csv_name = os.path.basename(pop_report_csv_file)
@@ -70,7 +64,6 @@ def write_to_csv(pop_report_csv_file, pop_csv_dict_of_df, pop_report_csv_path, n
         dict_writer.writerows(pop_csv_dict_of_df)
 
     print(f"Merged Pull Request Count CSV file done: {new_path_csv_output}")
-
 
 def merge_csv_pop_report(pop_report_csv_path, pr_history_csv_path, pop_file_suffix, pr_file_prefix):
     pop_report_csv_file_list = glob.glob(os.path.join(pop_report_csv_path, "*.csv"))
