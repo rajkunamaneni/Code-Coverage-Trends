7deb33af92f03c59e6dcadc35e7daba83f3ce80a
HC <51252433+bchou9@users.noreply.github.com>
2024-03-13 00:48:44 -0700

    Fix issue with non matching dates being zero

--- src/GetComulativePrHistoryData.py
+++ src/GetComulativePrHistoryData.py
@@ -22,17 +22,10 @@ def combine_pr_totals_with_pop_report(pr_history_csv_file_list, pr_file_prefix, 
                 pr_csv_df, username, repository = MergeCsvFiles.get_username_reponame_from_filename(pr_csv_file, pr_file_prefix)
 
                 for idx, pop_date_commit_dict_inst in enumerate(pop_csv_dict_of_df):
-                    if (pop_date_commit_dict_inst['Username'] == username) \
-                            and (pop_date_commit_dict_inst['Repository'] == repository):
-
-                        for created_at_pr_row in zip(pr_csv_df['created_at'].values):
-                            created_at_pr_row_zero = created_at_pr_row[0]
-
-                            timestamp = pop_date_commit_dict_inst['Timestamp']
-                            if timestamp == created_at_pr_row_zero:
-                                pop_csv_dict_of_df[idx]['Total Pull Requests Before Timestamp'] = \
-                                    get_comulative_pr_data(timestamp, pr_csv_df)
-                                break
+                    if (pop_date_commit_dict_inst['Username'] == username) and \
+                            (pop_date_commit_dict_inst['Repository'] == repository):
+                        pop_csv_dict_of_df[idx]['Total Pull Requests Before Timestamp'] = \
+                            get_comulative_pr_data(pop_date_commit_dict_inst['Timestamp'], pr_csv_df)
         except ValueError:
             print(f'repo used invalid naming practices, skipping: {pr_csv_file}')
             pass
@@ -51,8 +44,8 @@ def merge_csv_pop_report(pop_report_csv_path, pr_history_csv_path, pop_file_pref
             MergeCsvFiles.write_to_csv(pop_report_csv_file, pop_csv_dict_of_df, pop_report_csv_path, new_prefix)
 
 if __name__ == "__main__":
-    pop_report_csv_path = r'C:\Users\lenovoi7\Documents\GitHub\GitHub-Testing-Trends\data\Popularity_Reports_Medium_Star'
-    pr_history_csv_path = r'C:\Users\lenovoi7\Documents\GitHub\GitHub-Testing-Trends\data\Pull_Request_CSV\Pull_Request_History_Med_Star_Repo'
+    pop_report_csv_path = r'C:\Users\lenovoi7\Documents\GitHub\GitHub-Testing-Trends\data\Popularity_Reports_High_Star'
+    pr_history_csv_path = r'C:\Users\lenovoi7\Documents\GitHub\GitHub-Testing-Trends\data\Pull_Request_CSV\Pull_Request_History_High_Star_Repo'
     pop_file_prefix = r'WithPrandContributions_'
     pr_file_prefix = r'prs_per_day_'
     new_prefix = 'PrTotals'
